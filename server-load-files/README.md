19b7264f005d6f79dee7a42bac3f6534c180d3b0
Lucas Inocente <olucassanchez@gmail.com>
2019-07-24 02:06:11 -0300

    Add sample api to serve local md files

---
+++ server-load-files/README.md
@@ -0,0 +1,3 @@
+deno run --allow-net --allow-read ./server.js
+
+curl http://localhost:8000/hello
--- server-load-files/server.js
+++ server-load-files/server.js
@@ -1,17 +1,26 @@
 const { open, stdout } = Deno;
-import { serve } from "https://deno.land/std/http/server.ts";
-const s = serve("0.0.0.0:8000");
+import { serve, ServerRequest } from "https://deno.land/std@v0.9.0/http/server.ts";
+import { readFileStrSync, existsSync } from 'https://deno.land/std@v0.9.0/fs/mod.ts';
+
+const server = serve("0.0.0.0:8000");
 const directory = "./content";
 
 async function main() {
-  for await (const req of s) {
-    const { url } = req
+  for await (const req of server) {
+    const { url } = req;
     const filename = `${directory}${url}.md`;
-    const file = await open(filename);
-    await Deno.copy(stdout, file)
-    await req.respond({ body: new TextEncoder().encode(`You file: \n ${file} \n`) });
-    file.close()
+    if (existsSync(filename)) {
+      const file = readFileStrSync(filename);
+      const body = new TextEncoder().encode(file);
+      let headers = new Headers();
+      headers.set("Content-Type", "text/plain; charset=utf-8");
+      req.respond({ body, headers });
+    } else {
+      req.respond({ body: new TextEncoder().encode(`#404`) });
+    }
   }
 }
+
+console.log('Running: http://localhost:8000');
 
 main();
